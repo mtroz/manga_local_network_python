8e6839262ee3eba1b3d5c0869b145d0477f1d208
Administrator <julien.garderon@gmail.com>
2021-06-06 18:17:59 +0200

    commentaire

--- services/managerdb.py
+++ services/managerdb.py
@@ -2,10 +2,21 @@
 
 import sqlite3
 
+# [JG] Pense à rajouter toujours dans le module, un moyen de changer "l'origine" : c-à-d le chemin 
+# Certaines classes d'exception existent dans Python dont celle-là : https://docs.python.org/3/library/exceptions.html#FileNotFoundError  
+
+CheminLocal = "." # "." = dossier courant, tel que "./" 
+
 class ManageDB:
 
 	def __init__(self, database_name):
-		self.database_name = database_name
+		global CheminLocal 
+		self.database_name = database_name 
+		self.chemin = f"{CheminLocal}/{self.database_name}" 
+		if not os.path.isfile( self.chemin ): 
+			raise FileNotFoundError( 
+				f"la base n'existe pas à '{self.chemin}' : impossible d'utiliser une base non-initialisée" 
+			) 
 		self.con = sqlite3.connect(self.database_name)
 	
 	def cursor(self):
